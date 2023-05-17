ad8fec202f22070ed5a49ee141aac72a2e480736
Jim Cronqvist <jim.cronqvist@gmail.com>
2023-05-17 13:55:12 +0200

    Initial commit

---
+++ helm-create.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+set -eo pipefail
+
+CHARTNAME="$1"
+FOLDER="${2:-generated}"
+
+if [[ $# -lt 1 ]]; then
+    echo "You have not passed any arguments, use this script like this:"
+    echo "'bash helm-create.sh <CHARTNAME>'"
+    echo "'bash helm-create.sh <CHARTNAME> ../helm-charts/charts'"
+    echo ""
+    exit 1
+fi
+
+if [[ "$CHARTNAME" =~ ^[a-zA-Z0-9._-]+$ ]]; then
+    echo "The chart name provided is valid"
+else
+    echo "The chart name contains illegal characters"
+    exit 1
+fi
+
+echo "Creating the folder for the new chart"
+mkdir -p "./$FOLDER"
+mkdir "./$FOLDER/$CHARTNAME"
+
+echo "Copying the new chart"
+cp -R ./CHARTNAME/* "./$FOLDER/$CHARTNAME/"
+
+echo "Updating the <CHARTNAME> to the provided name '$CHARTNAME'"
+find "./$FOLDER/$CHARTNAME/" -type f -exec sed -i "s/CHARTNAME/$CHARTNAME/g" $i {} +
